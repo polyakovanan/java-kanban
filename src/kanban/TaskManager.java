2bf38a00a931c46b47f814958f551b6038e59129
PolyakovAn <PolyakovAn@terralink-global.com>
2024-09-24 13:14:26 +0500

    Реорганизовал структуру хранения задач, заменив три мапы на мапу мап. Вынес некоторые крупные куски кода в подметоды (обновление/создание подзадач у эпика) Небольшие правки тут и там

--- src/kanban/TaskManager.java
+++ src/kanban/TaskManager.java
@@ -5,35 +5,23 @@ import kanban.Tasks.Subtask;
 import kanban.Tasks.Task;
 import kanban.Tasks.TaskType;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class TaskManager {
 
     private static int idCounter = 0; //id задачи уникален между всеми существующими задачами независимо от типа
-    private HashMap<Integer, Task> taskStorage; //три хэш-мапы под каждый тип задач. Не придумал пока другого решения,
-    private HashMap<Integer, Task> subtaskStorage; //которое не затруднит последующую работу по id задачи.
-    private HashMap<Integer, Task> epicStorage; //возможно, упускаю какое-то изящное решение с вложенными коллекциям
+    private HashMap<TaskType, HashMap<Integer, Task>> taskStorage;
 
     public TaskManager(){
         taskStorage = new HashMap<>();
-        subtaskStorage = new HashMap<>();
-        epicStorage = new HashMap<>();
+        for (TaskType type : TaskType.values())
+            taskStorage.put(type, new HashMap<>());
     }
 
     //Вернем полностью хэш-мапу с целевым типом задачи
-    public HashMap<Integer, Task> getTasksByType(TaskType type){
-        switch (type) {
-            case EPIC -> {
-                return epicStorage;
-            }
-            case TASK -> {
-                return taskStorage;
-            }
-            case SUBTASK -> {
-                return subtaskStorage;
-            }
-        }
-        return null;
+    public Map<Integer, Task> getTasksByType(TaskType type){
+        return taskStorage.get(type);
     }
 
     /*Полностью очистим хэш-мапу с целевым типом задачи.
@@ -41,98 +29,75 @@ public class TaskManager {
     * Сейчас нет сценариев с false, но это может быть заготовкой для обработки
     * ошибок в новой потенциальной логике.*/
     public boolean removeTasksByType(TaskType type){
-        switch (type) {
-            case EPIC -> {
-                epicStorage.clear();
-                //если очистили все эпики, то все подзадачи тоже осиротели. Очистим в них ссылки на эпики
-                for (Integer id : subtaskStorage.keySet()){
-                    Subtask subtask = (Subtask) subtaskStorage.get(id);
-                    subtask.setEpic(null);
-                }
+        HashMap<Integer, Task> tasks = taskStorage.get(type);
+        if (type == TaskType.EPIC) {
+            //Если очистили все эпики, то все подзадачи тоже осиротели. Очистим в них ссылки на эпики
+            for (Integer id : tasks.keySet()) {
+                Subtask subtask = (Subtask) tasks.get(id);
+                subtask.setEpic(null);
             }
-            case TASK -> {
-                taskStorage.clear();
-            }
-            case SUBTASK -> {
-                subtaskStorage.clear();
-                //если очистили все подзадачи, то все эпики тоже опустели. Очистим в них ссылки на подзадачи
-                for (Integer id : epicStorage.keySet()){
-                    Epic epic = (Epic) epicStorage.get(id);
-                    epic.clearSubtasks();
-                }
+        } else if (type == TaskType.SUBTASK) {
+            //Если очистили все подзадачи, то все эпики тоже опустели. Очистим в них ссылки на подзадачи
+            for (Integer id : tasks.keySet()){
+                Epic epic = (Epic) tasks.get(id);
+                epic.clearSubtasks();
             }
         }
 
+        tasks.clear();
         return true;
     }
 
-    /*Дадим возможножность фронту запросить задачу по ее id и типу,
+    /*Дадим возможность фронту запросить задачу по ее id и типу,
     * чтобы сократить время поиска по трем коллекциям */
     public Task getTaskByIdAndType(int id, TaskType type){
-        switch (type) {
-            case EPIC -> {
-                return epicStorage.get(id);
-            }
-            case TASK -> {
-                return taskStorage.get(id);
-            }
-            case SUBTASK -> {
-                return subtaskStorage.get(id);
-            }
-        }
-        return null;
+        return taskStorage.get(type).get(id);
     }
 
     /*Или просто по id, если фронту неизвестен тип задачи.
     * Тогда пройдемся по всем типам и вернем задачу как только она
     * где-то нашлась или null, если id еще не завели*/
     public Task getTaskById(int id){
-        Task task = null;
         for (TaskType type : TaskType.values()) {
-            task = getTaskByIdAndType(id, type);
+            Task task = getTaskByIdAndType(id, type);
             if (task != null){
                 return task;
             }
         }
-        return task;
+        return null;
     }
 
     /*Аналогичная история. Даем фронту создать задачу с указанием типа.
-    * Если такой id уже есть, то "СОЗДАТЬ" мы его его не можем
+    * Если такой id уже есть, то "СОЗДАТЬ" мы его не можем
     * (с точки зрения бизнес-логики - это уже будет обновление)
     * поэтому вернем false. Иначе true*/
     public boolean createTaskByType(Task task, TaskType type){
         int id = idCounter + 1;
+        HashMap<Integer, Task> tasks = taskStorage.get(type);
         switch (type) {
             case EPIC -> {
-                if (!epicStorage.containsKey(id)) {
+                if (!tasks.containsKey(id)) {
                     Epic epic = (Epic) task;
-                    epicStorage.put(++idCounter, epic);
+                    tasks.put(++idCounter, epic);
                     //для эпика также смотрим список его подзадач и обновляем в них ссылку на эпик
                     //(подразумеваем, что нельзя создать одним запросом и эпик, и его подзадачи)
                     //а еще сюда как будто транзакцию хочется, на случай если что-то пойдет не так в процессе
-                    if(epic.getSubtasks() != null){
-                        for (Integer subId : epic.getSubtasks().keySet()){
-                            Subtask subtask = (Subtask) subtaskStorage.get(subId);
-                            if(subtask != null){
-                                subtask.setEpic(epic);
-                            }
-                        }
-                    }
+                    createSubtasksByEpic(epic);
+
                     //пересчитываем статус эпика
                     epic.calcStatus();
                     return true;
                 }
             }
             case TASK -> {
-                if (!taskStorage.containsKey(id)) {
-                    taskStorage.put(++idCounter, task);
+                if (!tasks.containsKey(id)) {
+                    tasks.put(++idCounter, task);
                     return true;
                 }
             }
             case SUBTASK -> {
-                if (!subtaskStorage.containsKey(id)) {
-                    subtaskStorage.put(++idCounter, task);
+                if (!tasks.containsKey(id)) {
+                    tasks.put(++idCounter, task);
                     //для подзадачи находим ее эпик и добавляем в него ссылку на нее
                     //(подразумеваем, что нельзя создать одним запросом и подзадачу, и ее эпик)
                     Subtask subtask = (Subtask) task;
@@ -163,31 +128,15 @@ public class TaskManager {
     * то это не "ОБНОВЛЕНИЕ", а вставка. Поэтому вернем ошибку, если задачи
     * с таким id нет*/
     public boolean updateTaskByIdAndType(Task task, int id, TaskType type){
+        HashMap<Integer, Task> tasks = taskStorage.get(type);
         switch (type) {
             case EPIC -> {
-                if (epicStorage.containsKey(id)) {
+                if (tasks.containsKey(id)) {
                     //проверяем, сменился ли у эпика список подзадач
-                    Epic originalEpic = (Epic) epicStorage.get(id);
+                    Epic originalEpic = (Epic) tasks.get(id);
                     Epic newEpic = (Epic) task;
-                    Set<Integer> originalSubtasks = originalEpic.getSubtasks().keySet();
-                    Set<Integer> newSubtasks = newEpic.getSubtasks().keySet();
-
-                    //удаляем у всех подзадач, которых больше нет в списке у эпика ссылку на эпик
-                    for(Integer originalId : originalSubtasks){
-                        if (!newSubtasks.contains(originalId) && subtaskStorage.get(originalId) != null){
-                            Subtask subtask = (Subtask) subtaskStorage.get(originalId);
-                            subtask.setEpic(null);
-                        }
-                    }
-
-                    //добавляем у всех подзадач, которых теперь появились в списке у эпика ссылку на эпик
-                    for(Integer newId : newSubtasks){
-                        if (!originalSubtasks.contains(newId) && subtaskStorage.get(newId) != null){
-                            Subtask subtask = (Subtask) subtaskStorage.get(newId);
-                            subtask.setEpic(newEpic);
-                        }
-                    }
-                    epicStorage.put(id, newEpic);
+                    updateSubtasksByEpic(originalEpic, newEpic);
+                    tasks.put(id, newEpic);
 
                     //пересчитываем статус эпика
                     newEpic.calcStatus();
@@ -195,16 +144,16 @@ public class TaskManager {
                 }
             }
             case TASK -> {
-                if (taskStorage.containsKey(id)) {
-                    taskStorage.put(id, task);
+                if (tasks.containsKey(id)) {
+                    tasks.put(id, task);
                     return true;
                 }
             }
             case SUBTASK -> {
-                if (subtaskStorage.containsKey(id)) {
+                if (tasks.containsKey(id)) {
                     //проверяем, сменилась ли у подзадачи ссылка на эпик
                     //и если сменилась, то удаляем ее из старого эпика и добавляем в новый
-                    Subtask originalSubtask = (Subtask) subtaskStorage.get(id);
+                    Subtask originalSubtask = (Subtask) tasks.get(id);
                     Subtask newSubtask = (Subtask) task;
                     if (originalSubtask.getEpic().getId() != newSubtask.getEpic().getId()){
                         originalSubtask.getEpic().removeSubtask(originalSubtask.getId());
@@ -213,7 +162,7 @@ public class TaskManager {
                         newSubtask.getEpic().calcStatus();
                     }
 
-                    subtaskStorage.put(id, task);
+                    tasks.put(id, task);
                     return true;
                 }
             }
@@ -222,7 +171,7 @@ public class TaskManager {
         return false;
     }
 
-    /*Тут тоже тожем сами определить тип обновляемой задачи*/
+    /*Тут тоже можем сами определить тип обновляемой задачи*/
     public boolean updateTaskById(Task task, int id){
         if (task instanceof Epic) {
             return updateTaskByIdAndType(task, id, TaskType.EPIC);
@@ -236,35 +185,36 @@ public class TaskManager {
     /*И в очередной раз, следуя бизнес-логике, вернем ошибку, если удалять нечего.
     * Хоть этот процесс никак не вредит технической составляющей процесса*/
     public boolean deleteTaskByIdAndType(int id, TaskType type){
+        HashMap<Integer, Task> tasks = taskStorage.get(type);
         switch (type) {
             case EPIC -> {
-                if (epicStorage.containsKey(id)) {
+                if (tasks.containsKey(id)) {
                     //если удаляем эпик, то надо удалить ссылку на него из всех его подзадач
-                    Epic epic = (Epic) epicStorage.get(id);
+                    Epic epic = (Epic) tasks.get(id);
                     HashMap<Integer, Subtask> subtasks = epic.getSubtasks();
                     if (subtasks != null) {
                         for (Integer subId : subtasks.keySet()){
                             subtasks.get(subId).setEpic(null);
                         }
                     }
-                    epicStorage.remove(id);
+                    tasks.remove(id);
                     return true;
                 }
             }
             case TASK -> {
-                if (taskStorage.containsKey(id)) {
-                    taskStorage.remove(id);
+                if (tasks.containsKey(id)) {
+                    tasks.remove(id);
                     return true;
                 }
             }
             case SUBTASK -> {
-                if (subtaskStorage.containsKey(id)) {
+                if (tasks.containsKey(id)) {
                     //если подзадачу удалили, надо убрать ссылку на нее из ее эпика
-                    Subtask subtask = (Subtask) subtaskStorage.get(id);
+                    Subtask subtask = (Subtask) tasks.get(id);
                     subtask.getEpic().removeSubtask(id);
                     //пересчитываем статус эпика
                     subtask.getEpic().calcStatus();
-                    subtaskStorage.remove(id);
+                    tasks.remove(id);
                     return true;
                 }
             }
@@ -274,21 +224,51 @@ public class TaskManager {
 
     /*Если не отдали тип задачи - перебираем коллекции по существующим типам*/
     public boolean deleteTaskById(int id){
-        boolean isDeleted = false;
         for(TaskType type : TaskType.values()) {
-            isDeleted = deleteTaskByIdAndType(id, type);
-            if (isDeleted)
-                return isDeleted;
+            if (deleteTaskByIdAndType(id, type))
+                return true;
         }
 
-        return isDeleted;
+        return false;
     }
 
     /*Если есть такой эпик - отдаем его список подзадач*/
-    public HashMap<Integer, Subtask> getSubtasksByEpic(int id) {
-        if (epicStorage.containsKey(id)) {
-            return ((Epic) epicStorage.get(id)).getSubtasks();
+    public Map<Integer, Subtask> getSubtasksByEpic(int id) {
+        HashMap<Integer, Task> tasks = taskStorage.get(TaskType.EPIC);
+        if (tasks.containsKey(id)) {
+            return ((Epic) tasks.get(id)).getSubtasks();
         }
         return null;
+    }
+
+    private void createSubtasksByEpic(Epic epic){
+        if(epic.getSubtasks() != null){
+            for (Integer subId : epic.getSubtasks().keySet()){
+                Subtask subtask = (Subtask) taskStorage.get(TaskType.SUBTASK).get(subId);
+                if(subtask != null){
+                    subtask.setEpic(epic);
+                }
+            }
+        }
+    }
+    private void updateSubtasksByEpic(Epic originalEpic, Epic newEpic){
+        Set<Integer> originalSubtasks = originalEpic.getSubtasks().keySet();
+        Set<Integer> newSubtasks = newEpic.getSubtasks().keySet();
+
+        //удаляем у всех подзадач, которых больше нет в списке у эпика ссылку на эпик
+        for(Integer originalId : originalSubtasks){
+            if (!newSubtasks.contains(originalId) && taskStorage.get(TaskType.SUBTASK).get(originalId) != null){
+                Subtask subtask = (Subtask) taskStorage.get(TaskType.SUBTASK).get(originalId);
+                subtask.setEpic(null);
+            }
+        }
+
+        //добавляем у всех подзадач, которых теперь появились в списке у эпика ссылку на эпик
+        for(Integer newId : newSubtasks){
+            if (!originalSubtasks.contains(newId) && taskStorage.get(TaskType.SUBTASK).get(newId) != null){
+                Subtask subtask = (Subtask) taskStorage.get(TaskType.SUBTASK).get(newId);
+                subtask.setEpic(newEpic);
+            }
+        }
     }
 }
