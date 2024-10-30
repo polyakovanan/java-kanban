b8bd76901f2722347beef447daeb19caaa8aa0be
PolyakovAn <PolyakovAn@terralink-global.com>
2024-10-30 15:59:19 +0500

    Правки по 6 спринту, итерация 5. ...

--- src/ru/terralink/kanban/service/Managers.java
+++ src/ru/terralink/kanban/service/Managers.java
@@ -2,7 +2,9 @@ package ru.terralink.kanban.service;
 
 public class Managers {
 
-    private Managers() {}
+    private Managers() {
+
+    }
 
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
